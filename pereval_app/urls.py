7ad6394ac3b92043f66aa5f16ad0cf8e666c1e23
erdes10032 <erdes10032@gmail.com>
2025-12-12 17:27:23 +0300

    Change urls for 2 sprint

--- pereval_app/urls.py
+++ pereval_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import SubmitDataView
+from .views import (
+    SubmitDataView,
+    GetPerevalView,
+    UpdatePerevalView,
+    GetPerevalsByEmailView
+)
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
+    path('submitData/<int:pk>/', GetPerevalView.as_view(), name='get-pereval'),
+    path('submitData/<int:pk>/update/', UpdatePerevalView.as_view(), name='update-pereval'),
+    path('submitData/by-email/', GetPerevalsByEmailView.as_view(), name='get-perevals-by-email'),
 ]
